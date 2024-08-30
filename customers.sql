7d8b9472186e8667b6c11244384004a9afb46327
DataDivaNet <65778613+DataDivaNet@users.noreply.github.com>
2024-08-30 13:22:00 -0400

    Update customers.sql

--- customers.sql
+++ customers.sql
@@ -1,6 +1,13 @@
+SELECT  email,name,count(cus.id) orders, created_at,
 
-SELECT customer_id,name,email,created_at FROM `analytics-engineers-club.coffee_shop.orders` ord
+FROM `analytics-engineers-club.coffee_shop.orders` ord
+
+
 inner join `analytics-engineers-club.coffee_shop.customers` cus   on
 ord.customer_id = cus.id
+
+group by name,email, created_at
+order by created_at asc
 LIMIT 5
 ;
+
